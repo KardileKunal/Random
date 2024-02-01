2ebd6c867fe5fd9b3acd06d9a5258e662d33a02a
KardileKunal <kunalkardile35@gmail.com>
2024-02-01 14:36:18 +0530

    Initial commit

---
+++ hello.js
@@ -0,0 +1,56 @@
+console.log("hello\n");
+
+
+console.log("List of Fruits \n Apple \n Orange \n Mango");
+
+console.log(`List of Fruits 
+Apple
+Orange
+Mango`)
+
+var p=1000;
+var n=1;
+var r=7;
+var SI =`Simple Interest is ${(p * n * r)/100}`;
+console.log("Simple Intrest is"+ SI);
+
+console.log(`Menu Items
+SandWich
+French Fries
+Potato wedges
+Donuts
+Pizza`)
+
+
+// var x='Hello'
+// var x='Hello JS'
+// console.log(x);
+
+let x='Hello'
+{
+let x='Hello JS'
+}
+console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
